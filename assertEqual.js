6e5975be9fcc97107664a443efcd0754fc30053c
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-23 19:07:05 -0500

    Add assertEqual.js file to lotide

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✔✔✔ Assertion Passed: " + actual + " === " + expected);
+
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+
+
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Horse", "Horse");
+assertEqual("Dog", "Cat");
+assertEqual(3, 3);
+assertEqual(4, 5);

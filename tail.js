c05cdb284dc279bd3c37a08d7f3236c14214e158
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-23 21:51:53 -0500

    added a comment and adjustment to test cases

--- tail.js
+++ tail.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
 
   }
 };
-
+// Returns a new array with the first element of the input array removed
 const tail = function(arr) {
   return arr.slice(1);
 };
@@ -17,11 +17,13 @@ const tail = function(arr) {
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
+
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 assertEqual(tail(["Elephant"]).length, 0); //An array with only one element should yield an empty array for its tail
 assertEqual(tail([]).length, 0); //An empty array should yield an empty array for its tail
 
+const wordsTail = tail(words);
 assertEqual(tail(words)[0], "Lighthouse"); // first element of returned array should be second element of argument array
 assertEqual(tail(words).length, words.length - 1); // tail should have one element less than its input

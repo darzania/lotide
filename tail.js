7a1df87fda39123e5a71e1ba6369682aebf9286d
Teodor Gumovsky <124521812+darzania@users.noreply.github.com>
2025-02-24 00:10:55 -0500

    Update tail.js

--- tail.js
+++ tail.js
@@ -15,20 +15,20 @@ const tail = function(arr) {
 
 
 
-// Test case: Check the original array
+// Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const wordsTail = tail(words); // Store the result of tail(words) in a variable
 assertEqual(words.length, 3); // Original array should still have 3 elements!
 
-// Test case: Check that tail of an array with one element is an empty array
+// Check that tail of an array with one element is an empty array
 assertEqual(tail(["Elephant"]).length, 0);
 
-// Test case: Check that tail of an empty array is an empty array
+// Check that tail of an empty array is an empty array
 assertEqual(tail([]).length, 0);
 
-// Test case: Check that the first element of the returned array is the second element of the input array
+// Check that the first element of the returned array is the second element of the input array
 assertEqual(wordsTail[0], "Lighthouse");
 
-// Test case: Check that the returned array has one less element than the input array
+// Check that the returned array has one less element than the input array
 assertEqual(wordsTail.length, words.length - 1);
 

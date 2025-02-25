e19db1634a6984be76401d35263945fef345b713
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-25 17:38:31 -0500

    applied a new test case

--- middle.js
+++ middle.js
@@ -42,7 +42,8 @@ const middle = function(array) {
 
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([10, 20, 30, 40, 50, 60])); // => [30, 40]
 console.log(middle([10, 20, 30, 40, 50, 60, 70])); // => [40]
+
+assertArraysEqual(middle([1, 2, 3]), [2]); // Should Pass

342b94b044fcac711924888fffdc07614731a545
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-25 17:22:34 -0500

    implemented middle function

---
+++ middle.js
@@ -0,0 +1,50 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
+
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔✔✔  Assertion Passed: Arrays ${actual} and ${expected} are the same.`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: Arrays ${actual} !== ${expected} are not the same.`);
+  }
+};
+
+const middle = function(array) {
+  let newArray = [];
+
+  if (array.length < 3) {
+    return newArray;
+  }
+
+  if (array.length % 2 !== 0) {
+    const middle = Math.floor(array.length / 2);
+    newArray.push(array[middle]);
+  } else {
+    const middle = (array.length / 2) - 1;
+    const nextMiddle = array.length / 2;
+    newArray.push(array[middle], array[nextMiddle]);
+  }
+
+  return newArray;
+
+};
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([10, 20, 30, 40, 50, 60])); // => [30, 40]
+console.log(middle([10, 20, 30, 40, 50, 60, 70])); // => [40]

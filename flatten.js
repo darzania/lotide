dafc87f70cf8cc8067c91d636ec8276bf5e67f9c
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-25 14:09:37 -0500

    implemented flatten function

---
+++ flatten.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
+
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔✔✔  Assertion Passed: Arrays ${actual} and ${expected} are the same.`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: Arrays ${actual} !== ${expected} are not the same.`);
+  }
+};
+
+const flatten = function(arr) {
+  let flatArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flatArray.push(...arr[i]);
+
+    } else {
+      flatArray.push(arr[i]);
+    }
+  }
+  
+  return flatArray;
+
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(flatten(["cat", "dog", ["mouse", "tiger"], "cow", ["raven"]])); // => ['cat', 'dog', 'mouse', 'tiger', 'cow', 'raven']
+
+(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])); // => Should Pass

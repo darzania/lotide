09fb010dbb6f78f9e3c370678a65bb9de54baad3
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-02-25 14:16:30 -0500

    added 1 more test case, removed unneeded parantheses

--- flatten.js
+++ flatten.js
@@ -40,5 +40,5 @@ const flatten = function(arr) {
 
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
 console.log(flatten(["cat", "dog", ["mouse", "tiger"], "cow", ["raven"]])); // => ['cat', 'dog', 'mouse', 'tiger', 'cow', 'raven']
-
-(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])); // => Should Pass
+console.log(flatten([[], 1, [2, 3], 4])); // => [1, 2, 3, 4]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => Should Pass

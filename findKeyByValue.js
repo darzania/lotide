2cc7dcdc09dc15e30cf702bbdbeacc579fbb9f6d
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-03-01 00:58:21 -0500

    should fail comment added

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -29,4 +29,4 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama"); // Should Fail

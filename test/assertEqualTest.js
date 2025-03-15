395b71075a3231649ba8a826e9f70f9d9434cd3b
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-03-15 02:47:22 -0400

    Added test directory and updated assertEqual and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Horse", "Horse");
+assertEqual("Dog", "Cat");
+assertEqual(3, 3);
+assertEqual(4, 5);

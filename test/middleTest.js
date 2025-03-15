5f7abfa81f28b7e3ccac6b0ee465088dedeca387
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-03-15 15:00:05 -0400

    added another test case that uses assertArraysEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -7,4 +7,5 @@ console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([10, 20, 30, 40, 50, 60])); // => [30, 40]
 console.log(middle([10, 20, 30, 40, 50, 60, 70])); // => [40]
 
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should Pass
 assertArraysEqual(middle([1, 2, 3]), [2]); // Should Pass

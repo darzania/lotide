24f62edc3706f8e8530cd57722e28063bb3b5c6e
Teodor Gumovsky <teodorgumovsky@gmail.com>
2025-03-15 18:31:39 -0400

    changed description for one of the tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -22,7 +22,7 @@ describe("#tail", () => {
     assert.deepEqual(wordsTail[0], "Lighthouse"); // Check the first element of the tail is the second element of the input array
   });
 
-  it("returns the second element of the input array as the first element of the tail", () => {
+  it("returns a tail with one less element than the input array", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     const wordsTail = tail(words); // Store the result of tail(words) in a variable
     assert.deepEqual(wordsTail.length, words.length - 1); // Check that the length of the tail is one less than the original array
